17a71bbf01da3bff686749c8ac9c746339ff7af2
Cyrus Parvereshi <cjparvereshi@ucdavis.edu>
2019-05-28 14:00:19 -0800

    starting function to combine DFs

--- spotifyAPI.R
+++ spotifyAPI.R
@@ -5,6 +5,7 @@ access_token <- get_spotify_access_token()
 
 
 beatles <- get_artist_audio_features('the beatles')
+View(beatles)
 library(tidyverse)
 library(knitr)
 
@@ -19,7 +20,7 @@ get_my_recently_played(limit = 5) %>%
   select(track.name, artist.name, track.album.name, played_at) %>% 
   kable()
 get_my_top_artists_or_tracks(type = 'artists', time_range = 'long_term', limit = 10) %>% 
-  select(name, genres) %>% 
+  select(name, genres) %>%
   rowwise %>% 
   mutate(genres = paste(genres, collapse = ', ')) %>% 
   ungroup %>% 
@@ -47,9 +48,56 @@ ggplot(joy, aes(x = valence, y = album_name)) +
 # features <- get_track_audio_features(tracks)
 
 aSong = get_track_audio_features("2oF7FZHIJbzjeEXZ3D0Ku4")
+
 quavo_id = "0VRj0yCOv2FXJNP47XQnx5"
 quavo = get_artist(quavo_id)
 quavo_albums = get_artist_albums(quavo_id)
 quavo_top = get_artist_top_tracks(quavo_id)$id #heres what we could use
 quavo_track_features = get_track_audio_features(quavo_top)
 #View(quavo_track_features)
+
+get_id <- function(URI) {
+  
+  spotifyID = strsplit(URI, ":")[[1]][3]
+  return(spotifyID)
+}
+#get starboy albums tracks
+starboy_id = get_id('spotify:album:2ODvWsOgouMbaA5xf0RkJe')
+starboy_tracks = get_album_tracks(starboy_id)
+starboy_track_names = starboy_tracks$name
+starboy_feats = get_track_audio_features(starboy_tracks$id)
+
+View(starboy_feats)
+
+
+# get_all_artists_tracks <- function(artist) {
+#   #tried to make my own function, doesn't really work
+#   albums = get_artist_albums(artist)$id
+#   album_tracks = get_album_tracks(albums)
+#   album_track_names = album_tracks$name
+#   album_feats = get_track_audio_features(album_tracks$id)
+#   return(album_feats)
+# }
+#get_all_artists_tracks(quavo_id) 
+
+
+#this is all I had to do lmao
+drake = get_artist_audio_features("Drake")
+theWknd = get_artist_audio_features("The Weeknd")
+colnames(theWknd)
+
+library(reticulate)
+py_save_object(drake, "drake.pkl")
+saved_drake = py_load_object("drake.pkl")
+library(data.table)
+a = setDT(drake)
+setDT(theWknd)
+drake$acousticness
+get_artist_audio_features("  cardi b ")
+rbind(drake, theWknd)
+
+smash_artists <- function(artistList){
+  for i in seq(artistList){
+    setDT(i)
+  }
+}
